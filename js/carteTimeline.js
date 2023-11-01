9fb7898b974f95fa878c62883d85c23c3344169f
arkunis <skyled.mgt@gmail.com>
2023-11-01 17:30:17 +0100

    Update carteTimeline.js

--- js/carteTimeline.js
+++ js/carteTimeline.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
             const geometry = new THREE.SphereGeometry(2, 15, -40);
-            const material = new THREE.MeshBasicMaterial({ color: 0xEAD1DC, wireframe: true })
+            const material = new THREE.MeshBasicMaterial({ color: 0xf200ff, wireframe: true })
             const cylinder = new THREE.Mesh(geometry, material)
 
             scene.add(cylinder)
